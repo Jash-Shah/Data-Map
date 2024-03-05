551d7517ec1cdfd91ee11bacb5bcda52110423cb
Jash <jash@analyticsverse.com>
2024-03-06 00:40:02 +0530

    Added sklearn model and pandas dataframe backend instead of sqlite

--- app.py
+++ app.py
@@ -1,9 +1,39 @@
 from flask import Flask, jsonify
 import sqlite3
+import pickle
+import pandas as pd
+import re
+import numpy as np
 
 from sqlalchemy import null
 
 app = Flask(__name__)
+
+model_knn = pickle.load(open("data/model.pkl", "rb"))
+movies_df = pd.read_csv("data/movie_titles.csv", index_col="id")
+encoded_features = pd.read_csv("data/encoded_features.csv", index_col="id")
+
+# A function to clean the search string
+def clean_string(input_string):
+    # Remove characters that are not letters or numbers
+    cleaned_string = re.sub(r'[^a-zA-Z0-9]', '', input_string)
+    # Remove whitespaces
+    cleaned_string = cleaned_string.replace(' ', '')
+    # Convert to lowercase
+    cleaned_string = cleaned_string.lower()
+    return cleaned_string
+
+# function to recommend top 10 movies
+def recommend_movies(movie,nn_data,orig_data):
+    basemovie = orig_data.iloc[orig_data[orig_data["original_title"].apply(lambda x: clean_string(x)).str.contains(clean_string(movie))].original_title.str.len().sort_values().index[0]].to_frame().T
+    movie_index=nn_data[orig_data.original_title==basemovie.original_title.values[0]].index
+    _, indices = model_knn.kneighbors(np.array(nn_data.iloc[movie_index]).reshape(1, -1),n_neighbors=15)
+
+    out=orig_data.iloc[indices[0]]
+    return out
+
+def search_movie(movie):
+    return movies_df["original_title"].apply(clean_string).str.contains(clean_string(movie)).any()
 
 @app.route('/')
 def home():
@@ -14,29 +44,19 @@ Take in users movie as argument and returns a JSON object in the form of a d3.js
 '''
 @app.route("/similar_to/<string:user_movie>", methods =["GET"])
 def similar_to(user_movie):
-    movie = user_movie.lower() #converting user enetered string to lowercase
-    #  Connecting to db and initialising cursor
-    conn = sqlite3.connect('Movies.db')
-    cursor = conn.cursor()
-    #  Getting the 20 similar movies to users inputted choice in order
-    similar_movies = cursor.execute("SELECT * FROM Movies WHERE LOWER(title)=?",(movie, ))
-    similar = similar_movies.fetchall()
-    # for row in similar:
-    #     print(dict(row))
-    res = {}
-    final = {"nodes" : [] , "links" : []} # Format of JSON object for d3.js Graph
-    val = 255
-    # r,b = 255,255
     try:
-        for i in range(2,len(similar[0])):
-            res[i] =  similar[0][i]
-        for rank,mov in res.items():
+        similar = recommend_movies(user_movie, encoded_features, movies_df)
+        similar = similar["original_title"].tolist()
+        final = {"nodes" : [] , "links" : []} # Format of JSON object for d3.js Graph
+        val = 255
+        # r,b = 255,255
+        for mov in similar[1:]:
             node = {"id" : mov , "color" : "white"}
-            link = {"source" : similar[0][1], "target" : mov, "value" : val}
+            link = {"source" : similar[0], "target" : mov, "value" : val}
             final["nodes"].append(node)
             final["links"].append(link)
             val= val - 15
-        final["nodes"].append({"id": similar[0][1], "color" : "rgba(165, 85, 253, 1)"})
+        final["nodes"].append({"id": similar[0], "color" : "rgba(165, 85, 253, 1)"})
     except:
         final = {"status" : "error404", "message" : "Movie not Found"}
     final =  jsonify(final)
@@ -49,13 +69,8 @@ Returns 0 or 1 depending on wheter given movie is in the db or not
 '''
 @app.route("/is_in/<string:user_movie>", methods = ["GET"])
 def is_in(user_movie):
-    movie = user_movie.lower() #converting user enetered string to lowercase
-    conn = sqlite3.connect('Movies.db')
-    cursor = conn.cursor()
-    is_movie =  cursor.execute("SELECT COUNT(title) FROM Movies WHERE LOWER(title)=?",(movie,))
-    is_movie = is_movie.fetchone()
-    # print(is_movie)
-    is_movie = jsonify(is_movie)
+    is_movie = search_movie(user_movie)
+    is_movie = jsonify(bool(is_movie))
     is_movie.headers.add("Access-Control-Allow-Origin","*")
     return is_movie
 
